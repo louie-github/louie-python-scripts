f5b1fc8499e15e84eac081ef5b2f5128bdbe31d3
Louie <areve.btorres@gmail.com>
2020-07-16 23:26:39 +0800

    Add more options

--- pip_upgrade_all.py
+++ pip_upgrade_all.py
@@ -42,6 +42,16 @@ if __name__ == "__main__":
         description="Generate a command to update all installed pip packages"
     )
     parser.add_argument(
+        "--prefix",
+        help=(
+            "If specified, the command to prefix before all pip <command> strings.\n"
+            "Overrides --use_py if specified. Example: --prefix 'python -m'"
+        ),
+        default=None,
+        metavar="COMMAND",
+        dest="prefix",
+    )
+    parser.add_argument(
         "--use-py",
         help="Use py launcher (Windows only, ignored otherwise)",
         action="store_true",
@@ -49,16 +59,23 @@ if __name__ == "__main__":
         dest="use_py",
     )
     args = parser.parse_args()
+    # Define base commands
+    pip_list_command = PIP_LIST_COMMAND
+    pip_upgrade_command = PIP_UPGRADE_COMMAND
     # Remove use_py argument if not running on Windows
     if sys.platform != "win32":
         args.use_py = False
-    # Append py launcher if necessary
+    # Override py launcher with prefix
+    if args.prefix:
+        args.use_py = False
+    # Append py launcher / prefixes if necessary
     if args.use_py:
-        pip_list_command = _PY_LAUNCHER_COMMAND + PIP_LIST_COMMAND
-        pip_upgrade_command = _PY_LAUNCHER_COMMAND + PIP_UPGRADE_COMMAND
-    else:
-        pip_list_command = PIP_LIST_COMMAND
-        pip_upgrade_command = PIP_UPGRADE_COMMAND
+        pip_list_command = _PY_LAUNCHER_COMMAND + pip_list_command
+        pip_upgrade_command = _PY_LAUNCHER_COMMAND + pip_upgrade_command
+    elif args.prefix:
+        prefix = shlex.split(args.prefix)
+        pip_list_command = prefix + pip_list_command
+        pip_upgrade_command = prefix + pip_upgrade_command
     # Main code
     packages = get_packages(cmd=pip_list_command)
     print(generate_upgrade_command(packages, prefix=pip_upgrade_command))
