482b45951456f1279ca75d2eb18f1072632e0120
Louie <areve.btorres@gmail.com>
2020-07-17 01:48:22 +0800

    Add more code

--- piptools/upgrade_all.py
+++ piptools/upgrade_all.py
@@ -14,6 +14,7 @@ Options:
 Output options:
     -v, --verbose    Print verbose output (useful for debugging.)
     -q, --quiet      Suppress the output of subprocess calls, i.e. quiet mode.
+                     Note: This is currently unused; '--run' will always output.
 
 Script options:
     -r, --run        Run the resulting command instead of printing it. (DANGEROUS)
@@ -51,6 +52,8 @@ except AttributeError:
         return " ".join(shlex.quote(arg) for arg in split_command)
 
     shlex.join = _shlex_join
+# By default, verbose printing is off.
+vprint = lambda *args, **kwargs: None  # noqa E731
 
 
 def _remove_whitespace(text, whitespace=string.whitespace):
@@ -80,16 +83,20 @@ def get_packages(
     list_command=PIP_LIST_COMMAND, regex=PIP_LIST_REGEX, skip_checks=False
 ):
     sys_encoding = sys.stdout.encoding
+    vprint(f"Running 'pip list' command: {list_command}")
     output = subprocess.check_output(list_command)
     # Parse output into a list of lines and decode into str; strip
     lines = [line.decode(sys_encoding).strip() for line in output.splitlines()]
     # Check if output has the expected prefix lines
     if not skip_checks:
         passed, checked_lines = _check_pip_list_prefix(lines)
+        vprint(f"Passed 'pip list' output checks on lines: {checked_lines}")
         if not passed:
             raise ValueError(
                 f"Expected a valid 'pip list' output, instead got: {checked_lines}"
             )
+    else:
+        vprint(f"Skipped 'pip list' output checks.")
     # Skip first two lines
     for line in lines[2:]:
         match = regex.match(line)
@@ -106,7 +113,9 @@ def generate_upgrade_command(
 ):
     # Get packages if not specified
     packages = packages if packages is not None else get_packages(*args, **kwargs)
-    output = list(upgrade_command) + list(packages)
+    packages = list(packages)
+    vprint(f"Found packages: {packages}")
+    output = list(upgrade_command) + packages
     if join_output:
         return shlex.join(output)
     else:
@@ -115,14 +124,20 @@ def generate_upgrade_command(
 
 # ----- INTERACTIVE CODE -----
 def main(args: Dict[str, Any] = None):
+    global vprint
     # Use duplicate imports, just to be safe :>
     import sys  # noqa
 
     if not args:
         args = docopt(__doc__, version=f"louie.piptools.upgrade_all: {__version__}")
+    # Enable verbose printing if necessary
+    if args["--verbose"]:
+        vprint = print
     # Define base commands
     pip_list_command = PIP_LIST_COMMAND
     pip_upgrade_command = PIP_UPGRADE_COMMAND
+
+    # ---> ARGUMENT PARSING
     # Remove use_py argument if not running on Windows
     if not sys.platform.startswith("win32"):
         args["--use-py"] = False
@@ -142,6 +157,11 @@ def main(args: Dict[str, Any] = None):
     # Add prefix to all commands
     pip_list_command = prefix + pip_list_command
     pip_upgrade_command = prefix + pip_upgrade_command
+
+    # Debug print the commands
+    vprint(f"pip_list_command: {pip_list_command}")
+    vprint(f"pip_upgrade_command: {pip_upgrade_command}")
+
     # Main code
     packages = get_packages(list_command=pip_list_command)
     # Run if needed
