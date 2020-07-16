b23ce2fc52a06b288a4954eeb0aef12cc9a0e11d
Louie <areve.btorres@gmail.com>
2020-07-16 23:39:24 +0800

    Make into package

--- piptools/upgrade_all.py
+++ piptools/upgrade_all.py
@@ -6,13 +6,16 @@ import shlex
 import subprocess
 import sys
 
-_PY_LAUNCHER_COMMAND = ["py", "-3.8", "-m"]
+__all__ = ["get_packages", "generate_upgrade_command"]
+
+
+PY_LAUNCHER_COMMAND = ["py", "-3.8", "-m"]
 PIP_LIST_COMMAND = ["pip", "list"]
 PIP_UPGRADE_COMMAND = ["pip", "install", "--upgrade", "--no-cache-dir"]
 PIP_LIST_REGEX = re.compile(r"([\S]*) *(.*)")
 
 
-def get_packages(cmd=PIP_LIST_COMMAND, regex=PIP_LIST_REGEX):
+def get_packages(list_command=PIP_LIST_COMMAND, regex=PIP_LIST_REGEX):
     sys_encoding = sys.stdout.encoding
     output = subprocess.check_output(cmd)
     # Parse output into a list of lines and decode into str; strip
@@ -27,38 +30,44 @@ def get_packages(cmd=PIP_LIST_COMMAND, regex=PIP_LIST_REGEX):
             yield match.group(1)
 
 
-def generate_upgrade_command(packages, prefix=PIP_UPGRADE_COMMAND):
+def generate_upgrade_command(
+    packages=None, upgrade_command=PIP_UPGRADE_COMMAND, *args, **kwargs
+):
+    packages = packages if packages is not None else get_packages(*args, **kwargs)
     # Quote every package for safety, if needed
     packages_str = " ".join([shlex.quote(package) for package in packages])
     prefix = shlex.join(prefix)
     return f"{prefix} {packages_str}"
 
 
-if __name__ == "__main__":
-    # Ignore duplicate imports
+# ----- INTERACTIVE CODE -----
+def main(args=None):
+    # Use duplicate imports, just to be safe :>
     import argparse, sys  # noqa
 
-    parser = argparse.ArgumentParser(
-        description="Generate a command to update all installed pip packages"
-    )
-    parser.add_argument(
-        "--prefix",
-        help=(
-            "If specified, the command to prefix before all pip <command> strings.\n"
-            "Overrides --use_py if specified. Example: --prefix 'python -m'"
-        ),
-        default=None,
-        metavar="COMMAND",
-        dest="prefix",
-    )
-    parser.add_argument(
-        "--use-py",
-        help="Use py launcher (Windows only, ignored otherwise)",
-        action="store_true",
-        default=True,  # We will disable this later if not on Windows
-        dest="use_py",
-    )
-    args = parser.parse_args()
+    if not args:
+        # Main arguments
+        parser = argparse.ArgumentParser(
+            description="Generate a command to update all installed pip packages"
+        )
+        parser.add_argument(
+            "--prefix",
+            help=(
+                "If specified, the command to prefix before all pip <command> strings.\n"
+                "Overrides --use_py if specified. Example: --prefix 'python -m'"
+            ),
+            default=None,
+            metavar="COMMAND",
+            dest="prefix",
+        )
+        parser.add_argument(
+            "--use-py",
+            help="Use py launcher (Windows only, ignored otherwise)",
+            action="store_true",
+            default=True,  # We will disable this later if not on Windows
+            dest="use_py",
+        )
+        args = parser.parse_args()
     # Define base commands
     pip_list_command = PIP_LIST_COMMAND
     pip_upgrade_command = PIP_UPGRADE_COMMAND
@@ -70,12 +79,16 @@ if __name__ == "__main__":
         args.use_py = False
     # Append py launcher / prefixes if necessary
     if args.use_py:
-        pip_list_command = _PY_LAUNCHER_COMMAND + pip_list_command
-        pip_upgrade_command = _PY_LAUNCHER_COMMAND + pip_upgrade_command
+        pip_list_command = PY_LAUNCHER_COMMAND + pip_list_command
+        pip_upgrade_command = PY_LAUNCHER_COMMAND + pip_upgrade_command
     elif args.prefix:
         prefix = shlex.split(args.prefix)
         pip_list_command = prefix + pip_list_command
         pip_upgrade_command = prefix + pip_upgrade_command
     # Main code
-    packages = get_packages(cmd=pip_list_command)
-    print(generate_upgrade_command(packages, prefix=pip_upgrade_command))
+    packages = get_packages(list_command=pip_list_command)
+    print(generate_upgrade_command(packages, upgrade_command=pip_upgrade_command))
+
+
+if __name__ == "__main__":
+    main()
