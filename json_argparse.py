81a0c20beea4af304d6bbc740fe9c83934689a11
Louie <areve.btorres@gmail.com>
2020-07-19 17:46:57 +0800

    Use class

--- json_argparse.py
+++ json_argparse.py
@@ -8,38 +8,41 @@ import json
 from typing import TextIO, Union
 
 
-def parse_json(json_data: Union[str, bytes, bytearray, TextIO]):
-    # Try loading as file first, then as str
-    try:
-        json.load(json_data)
-    except AttributeError:
-        try:
-            json.loads(json_data)
-        except json.JSONDecodeError:
-            raise ValueError("Expected a JSON file object or valid JSON data")
-
-
-def _parse_arguments(data: dict):
-    for name, kwargs in data.iteritems():
-        # Processing steps:
-        # 1. Replace the str 'argparse.REMAINDER' with the actual argparse.REMAINDER
-        if kwargs.get("nargs") == "argparse.REMAINDER":
-            kwargs["nargs"] = argparse.REMAINDER
-
-        yield name, kwargs
-
-
-def make_argparse(json_data: dict):
-    # First, get all the non-arguments
-    json_data = json_data.copy()
-    argparse_args = inspect.getfullargspec(argparse.ArgumentParser)
-    arguments = []
-    for key in json_data.keys():
-        if key == "arguments":
-            arguments = _parse_arguments(json_data[key])
-
-
 class JSONArgumentParser(argparse.ArgumentParser):
-    def __init__(self, json_file, *args, **kwargs):
-        json_data = parse_json(json_file)
+    def __init__(self, json_file: Union[str, TextIO], *args, **kwargs):
+        # Case 1: json_data is a file object
+        try:
+            json.load(json_file)
+        except AttributeError:
+            # Case 2: json_data is a filename
+            try:
+                with open(json_file) as f:
+                    json_data = json.load(f)
+            except FileNotFoundError:
+                # Case 3: json_data is str with JSON data
+                try:
+                    json_data = json.loads(json_file)
+                except json.JSONDecodeError:
+                    raise ValueError(
+                        "Expected a file-like object, filename, or str with JSON data for argument 'json_file'"
+                    )
+        self._parse_parser(json_data)
+        self._parse_arguments(json_data)
+
+    def _parse_parser(self, data: dict):
+        return super().__init__(self, **data)
+
+    def _parse_arguments(self, data: dict):
+        for name, kwargs in data.items():
+            # Processing steps:
+            # 1. Replace the str 'argparse.REMAINDER' with the actual argparse.REMAINDER
+            if kwargs.get("nargs") == "argparse.REMAINDER":
+                kwargs["nargs"] = argparse.REMAINDER
+            # 2. Get the additional aliases of the option
+            names = [name]
+            names.append(kwargs.get("aliases", []))
+
+            # FINAL: Call add_argument  with the option name(s) as positional
+            # arguments and all other arguments as keyword arguments
+            self.add_argument(*names, **kwargs)
 
