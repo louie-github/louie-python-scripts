95b5564fe7894ea0bc9924c594d4f4dffb4a3eb7
Louie <areve.btorres@gmail.com>
2020-07-26 23:49:22 +0800

    Allow JSONArgumentParser to be imported directly from core

--- __main__.py
+++ __main__.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 
 # Copyright 2020 louie-github
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,7 +25,7 @@ from typing import List
 sys.path.append(os.path.dirname(__file__))
 
 # Import JSON argparse script
-from core.json_argparse import JSONArgumentParser  # noqa: E402
+from core import JSONArgumentParser  # noqa: E402
 
 json_file = os.path.join(os.path.dirname(__file__), "cli", "__main__.json")
 
--- pipup.py
+++ pipup.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 
 # Copyright 2020 louie-github
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -49,7 +49,7 @@ import sys
 from collections import deque
 from typing import Iterable, List
 
-from core.json_argparse import JSONArgumentParser
+from core import JSONArgumentParser
 
 fname = os.path.split(__file__)[-1].rsplit(".", 1)[0]
 CLI_JSON_CONFIG_FILE = os.path.join(os.path.dirname(__file__), "cli", f"{fname}.json")
